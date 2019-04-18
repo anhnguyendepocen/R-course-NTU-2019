1bcc6b44cd2eb12054485bda80060bf950072f4e
Daniel Vaulot <vaulot@gmail.com>
2019-04-18 15:56:05 +0800

    Remove the html

--- R-course-script.R
+++ R-course-script.R
@@ -8,5 +8,10 @@ knitr::purl(input="R/R-session-04-data_visualization.Rmd",
      output="R/R-session-04-data_visualization.R", 
      documentation=1)
 
+# In the file dummy, all eval=FALSE have been replaced by eval=TRUE and idem for purl=FALSE
+knitr::purl(input="R/R-session-05-mapping_dummy.Rmd", 
+     output="R/R-session-05-mapping.R", 
+     documentation=1)
+
 # Regular expression to clean the code
 #   -\s[^-]+-  replace by --
